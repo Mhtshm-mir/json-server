c72aa1e3baab36d34f1c90c75b24cf45827a77d1
Mhtshm-mir <mir.mohtashim66@gmail.com>
2023-03-21 12:27:04 +0530

    mock-6

--- signup.js
+++ signup.js
@@ -1,11 +1,19 @@
 const classes = (classes) => document.querySelector(classes);
-
+const id = (id) => document.getElementById(id);
 let signupBtn = classes(".signupBtn"),
   loginBtn = classes(".loginBtn"),
   moveBtn = classes(".moveBtn"),
   signup = classes(".signup"),
   login = classes(".login"),
-  form = classes("form");
+  form = classes("form"),
+  submitBtn = classes(".submitBtn"),
+  logsubmitBtn = classes(".log-submitBtn");
+
+let suUsername = id("signup-username"),
+  suEmail = id("signup-email"),
+  suPass = id("signup-password"),
+  logEmail = id("login-email"),
+  logPass = id("login-password");
 
 loginBtn.addEventListener("click", () => {
   moveBtn.classList.add("rightBtn");
@@ -18,3 +26,33 @@ signupBtn.addEventListener("click", () => {
   login.classList.remove("loginform");
   signup.classList.add("signupform");
 });
+
+submitBtn.addEventListener("click", (e) => {
+  submitData(e);
+});
+
+const submitData = (e) => {
+  e.preventDefault();
+  fetch("https://json-server-6ewh.onrender.com/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username: suUsername.value,
+      email: suEmail.value,
+      password: suPass.value,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    // Converting to JSON
+    .then((response) => response.json())
+
+    // Displaying results to console
+    .then((json) => console.log(json));
+};
+
+logsubmitBtn.addEventListener("click", (e) => {
+  loginData(e);
+});
+
+const loginData = (e) => {};
