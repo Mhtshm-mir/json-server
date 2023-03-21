3413123fd54a371b4143982b79ff82a89be2ef61
Mhtshm-mir <mir.mohtashim66@gmail.com>
2023-03-21 13:00:44 +0530

    mock-6

--- signup.js
+++ signup.js
@@ -49,10 +49,30 @@ const submitData = (e) => {
 
     // Displaying results to console
     .then((json) => console.log(json));
+  alert("signup successful, please login");
+  signup.classList.remove("signupform");
+  login.classList.add("loginform");
 };
 
 logsubmitBtn.addEventListener("click", (e) => {
   loginData(e);
 });
 
-const loginData = (e) => {};
+const loginData = (e) => {
+  e.preventDefault();
+  fetch(`https://json-server-6ewh.onrender.com/users?email=${logEmail.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      if (Object.keys(data).length === 0) {
+        alert("Please Enter valid email");
+      } else {
+        if (data[0].password === +logPass.value) {
+          alert("Log in Successfully");
+          window.location.href = "homepage.html";
+        } else {
+          alert("login failed");
+        }
+      }
+    });
+};
